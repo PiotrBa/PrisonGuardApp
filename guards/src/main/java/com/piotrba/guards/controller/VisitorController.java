e2d89f9f325bca0f88c84fc9da1cfd50253a2ebb
PiotrBa <piotr.balazyk@wp.pl>
2025-01-27 22:08:18 +0000

    Add @ResponseStatus for VisitorController

--- guards/src/main/java/com/piotrba/guards/controller/VisitorController.java
+++ guards/src/main/java/com/piotrba/guards/controller/VisitorController.java
@@ -5,6 +5,7 @@ import com.piotrba.guards.dto.visitor.VisitorDTO;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,30 +20,35 @@ public class VisitorController {
     private final VisitorClient visitorClient;
 
     @GetMapping("/all")
+    @ResponseStatus(HttpStatus.OK)
     public List<VisitorDTO> getAllVisitors() {
         logger.info("Received request to get all visitors");
         return visitorClient.getAllVisitors();
     }
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public VisitorDTO getVisitorById(@PathVariable Long id) {
         logger.info("Received request to get visitor by id: {}", id);
         return visitorClient.getVisitorById(id);
     }
 
     @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
     public VisitorDTO addVisitor(@RequestBody VisitorDTO visitorDTO) {
         logger.info("Received request to add a new visitor: {}", visitorDTO);
         return visitorClient.addVisitor(visitorDTO);
     }
 
     @PostMapping("/update/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public VisitorDTO updateVisitor(@PathVariable Long id, @RequestBody VisitorDTO visitorDTO) {
         logger.info("Received request to update visitor with id: {}", id);
         return visitorClient.updateVisitor(id, visitorDTO);
     }
 
     @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public void deleteVisitor(@PathVariable Long id) {
         logger.info("Received request to delete visitor with id: {}", id);
         visitorClient.deleteVisitor(id);
