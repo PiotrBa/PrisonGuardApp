d303b2e67a9b173c58d4cc12ee766b7f5c68aed3
PiotrBa <piotr.balazyk@wp.pl>
2025-01-29 19:19:05 +0000

    Refreshing FindGuardByIdTest class after changes in GuardService

--- guards/src/test/java/com/piotrba/guards/serviceImplTests/FindGuardByIdTest.java
+++ guards/src/test/java/com/piotrba/guards/serviceImplTests/FindGuardByIdTest.java
@@ -2,7 +2,7 @@ package com.piotrba.guards.serviceImplTests;
 
 import com.piotrba.guards.entity.Guard;
 import com.piotrba.guards.repo.GuardsRepository;
-import com.piotrba.guards.service.impl.GuardServiceImpl;
+import com.piotrba.guards.service.GuardService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -20,7 +20,7 @@ public class FindGuardByIdTest {
     @Mock
     private GuardsRepository guardsRepository;
     @InjectMocks
-    private GuardServiceImpl guardService;
+    private GuardService guardService;
     public Guard guard = Guard.builder()
             .id(1L)
             .firstName("Andy")
