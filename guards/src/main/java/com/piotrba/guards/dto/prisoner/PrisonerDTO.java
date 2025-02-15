46209a7b71836ec48e84d4ae735d8dc71696415a
PiotrBa <piotr.balazyk@wp.pl>
2025-02-15 16:58:53 +0000

    Integration tests for the getPrisonerById method

--- guards/src/main/java/com/piotrba/guards/dto/prisoner/PrisonerDTO.java
+++ guards/src/main/java/com/piotrba/guards/dto/prisoner/PrisonerDTO.java
@@ -2,11 +2,13 @@ package com.piotrba.guards.dto.prisoner;
 
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@Builder
 public class PrisonerDTO {
 
     private Long id;
