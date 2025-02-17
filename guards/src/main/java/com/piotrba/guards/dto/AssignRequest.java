1ea779ef5f51300020ec886f8b57b1df99d07005
PiotrBa <piotr.balazyk@wp.pl>
2025-02-17 23:16:41 +0000

    Unit test for the getVisitorsByPrisonerId method

--- guards/src/main/java/com/piotrba/guards/dto/AssignRequest.java
+++ guards/src/main/java/com/piotrba/guards/dto/AssignRequest.java
@@ -1,12 +1,14 @@
 package com.piotrba.guards.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class AssignRequest {
     private Long visitorId;
     private Long prisonerId;
