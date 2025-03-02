a9f00b2df8020f0fdcd319622e9fc2a92250c68f
PiotrBa <piotr.balazyk@wp.pl>
2025-03-02 12:58:16 +0000

    Refreshing the FindGuardByIdTest

--- guards/src/test/java/com/piotrba/guards/serviceImplTests/FindGuardByIdTest.java
+++ guards/src/test/java/com/piotrba/guards/serviceImplTests/FindGuardByIdTest.java
@@ -1,11 +1,13 @@
 package com.piotrba.guards.serviceImplTests;
 
+import com.piotrba.guards.client.PrisonerClient;
+import com.piotrba.guards.client.VisitorClient;
 import com.piotrba.guards.entity.Guard;
 import com.piotrba.guards.repo.GuardsRepository;
 import com.piotrba.guards.service.GuardService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -17,27 +19,38 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class FindGuardByIdTest {
+
     @Mock
     private GuardsRepository guardsRepository;
-    @InjectMocks
+    @Mock
+    private PrisonerClient prisonerClient;
+    @Mock
+    private VisitorClient visitorClient;
+
     private GuardService guardService;
-    public Guard guard = Guard.builder()
-            .id(1L)
-            .firstName("Andy")
-            .lastName("Smith")
-            .build();
+    private Guard guard;
+
+    @BeforeEach
+    public void setUp() {
+        guardService = new GuardService(guardsRepository, prisonerClient, visitorClient);
+        guard = Guard.builder()
+                .id(1L)
+                .firstName("Andy")
+                .lastName("Smith")
+                .build();
+    }
+
     @Test
-    public void findGuardById_whenGuardExists_shouldReturnGuard(){
+    public void findGuardById_whenGuardExists_shouldReturnGuard() {
         when(guardsRepository.findById(1L)).thenReturn(Optional.of(guard));
         Optional<Guard> result = guardService.findGuardById(1L);
         assertEquals(Optional.of(guard), result);
     }
 
     @Test
-    public void findGuardById_whenGuardDoesNotExist_shouldReturnEmptyOptional(){
+    public void findGuardById_whenGuardDoesNotExist_shouldReturnEmptyOptional() {
         when(guardsRepository.findById(2L)).thenReturn(Optional.empty());
         Optional<Guard> result = guardService.findGuardById(2L);
         assertFalse(result.isPresent());
     }
-
 }
